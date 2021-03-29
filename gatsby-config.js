de98dc460451d1f7a366e5a889f583be9dc4aa1d
langbl28 <langbl28@gmail.com>
2021-03-29 15:24:31 -0500

    Robots, sitemap, Purge CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,31 @@ module.exports = {
   siteMetadata: {
     title: "UX/UI Portfolo ",
     titleTemplate: "%s • Brandon Langer",
-    description:
-      "Creating empathic designs through modern UX/UI.",
+    description: "Creating empathic designs through modern UX/UI.",
     url: "https://www.brandonlanger.website", // No trailing slash allowed!
+    siteUrl: "https://www.brandonlanger.website",
     image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
     twitterUsername: "@hashtagbrownz",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://brandonlanger.website",
+        sitemap: "https://brandonlanger.website/sitemap.xml",
+        policy: [{ userAgent: "*", disallow: "/" }],
+      },
+    },
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: false, // Enable while using `gatsby develop`
+      }
+    }
+  ],
 }
